67647180a45f98eeff9b0548514e8ddf563569a0
root <root@ip-172-31-10-97.us-west-1.compute.internal>
2025-06-27 16:38:42 +0000

    adding green color to msg

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	  res.send('<h1 style="color: green;">Hello World from ECS Fargate!</h1>');
+	  res.send('<h1 style="color:green;">Hello World from ECS Fargate!</h1>');
 });
 
 app.listen(port, () => {
